497a1b40b77c2642521876573a8e17b861154c47
Colin Malcolm <colinmalcolm8@gmail.com>
2020-10-31 18:09:43 -0400

    code map complete

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1>Hello World </h1>
+      <h1>Hello World  </h1>
+
+    {/* Headeer  */}
+    {/* Title + Select input dropdown field */}
 
 
+
+    {/* Infoboxes */}
+    {/* Infoboxes */}
+    {/* Infoboxes */}
+
+    {/* Table */}
+     {/* Graph */}
+
+
+
+    {/* Map */}
 
 
 
