817942fb517ea93d41fa3690e432b23f0cbd3863
Colin Malcolm <colinmalcolm8@gmail.com>
2020-10-31 18:32:49 -0400

    initial dropdown menu completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
-
+import React from "react";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello World  </h1>
+      <h1>COVID Country  </h1>
+      <FormControl className="app__dropdown">
+        <Select
+        variant="outlined"
+        value="abc"
+        >
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Op 2</MenuItem>
+          <MenuItem value="worldwide">Op 3</MenuItem>
+          <MenuItem value="worldwide">Op 4</MenuItem>
+
+
+        </Select>
+
+      </FormControl>
 
     {/* Headeer  */}
     {/* Title + Select input dropdown field */}
